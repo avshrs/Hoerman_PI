3fa1eaa4e0e8988e497bc78e790bfa0fdf47370e
avshrs <avshrs@gmail.com>
2022-01-11 00:35:58 +0100

    up

--- src/hoer_read_status.cpp
+++ src/hoer_read_status.cpp
@@ -12,7 +12,7 @@ int main(){
    char serial_name[] = "/dev/ttyUSB0";
    int boudrate = 19200;
    door.open_serial(serial_name, boudrate);
-   door.loop();
+   //door.loop();
    current_door_state = door.get_state();
    
    std::string state = door.get_state_string();
