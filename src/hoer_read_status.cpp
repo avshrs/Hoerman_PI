0ceb3703e5db0463f22231e58a00626daf425f5d
avshrs <avshrs@gmail.com>
2022-01-11 00:38:56 +0100

    up

--- src/hoer_read_status.cpp
+++ src/hoer_read_status.cpp
@@ -1,18 +1,26 @@
 #include "hoermann.h"
 #include <iostream>
+// #include <thread>
+
 #include <unistd.h>
 Hoermann door;
 hoermann_state_t current_door_state = hoermann_state_unkown;
 hoermann_state_t last_door_state = hoermann_state_unkown;
 
 
+// void th1(){
+//     door.loop();
+// }
+
 
 int main(){
-   while(1){
+   
    char serial_name[] = "/dev/ttyUSB0";
    int boudrate = 19200;
    door.open_serial(serial_name, boudrate);
-   //door.loop();
+   
+   while(1){
+      door.loop();
    current_door_state = door.get_state();
    
    std::string state = door.get_state_string();
