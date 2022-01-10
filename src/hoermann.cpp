43283641a01394e935817512140589dbebe86c57
avshrs <avshrs@gmail.com>
2022-01-10 19:54:12 +0100

    up

--- src/hoermann.cpp
+++ src/hoermann.cpp
@@ -50,7 +50,7 @@ void Hoermann::open_serial(char * serial_name, int boudrate)
 bool Hoermann::read_rs232(void)
 {
     static uint8_t len = 0;
-    char buf[16]; 
+    char buf[16] = {0}; 
     serial.serial_read(buf, 16);
     for(int i=0; i<16 ; i++){
     std::cout << " 0x"<<std::setw(2) << std::setfill('0')<<std::hex << static_cast<int>(buf[i]);
