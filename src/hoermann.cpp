cc9e289e35f85f46e09098fa3fbce86233e19682
avshrs <avshrs@gmail.com>
2022-01-10 17:24:42 +0100

    up

--- src/hoermann.cpp
+++ src/hoermann.cpp
@@ -61,7 +61,7 @@ bool Hoermann::read_rs232(void)
     std::cout << std::endl;
 
           
-    if (buf[1] = 0x55) 
+    if (buf[1]== 0x55) 
     {
       std::cout<< std::endl<< std::endl << "found 0x55 in buffer "<< std::endl;
     }
