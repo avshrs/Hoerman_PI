47d9d91cbcf61eb5bead88c6cfa0010c5eac77f6
avshrs <avshrs@gmail.com>
2022-01-11 12:13:15 +0100

    up

--- src/hoer_read_status.cpp
+++ src/hoer_read_status.cpp
@@ -25,7 +25,7 @@ int main(){
    
    std::string state = door.get_state_string();
    
-std::cout << current_door_state << std::endl;
+// std::cout << current_door_state << std::endl;
 std::cout << state << std::endl;
    sleep(1);
    // door.trigger_action(hoermann_action_venting);
