d909dd8dd483a5a0eb21545d412fc83dc50dad44
avshrs <avshrs@gmail.com>
2022-01-11 11:55:23 +0100

    up

--- src/hoermann.cpp
+++ src/hoermann.cpp
@@ -75,6 +75,7 @@ bool Hoermann::read_rs232(void)
         
       }
       std::cout << "rx  0x"<<std::setw(2) << std::setfill('0')<<std::hex << static_cast<int>(rx_buffer[4]) << std::endl;
+      std::cout << "rx  0x"<<std::setw(2) << std::setfill('0')<<std::hex << static_cast<int>(calc_checksum(rx_buffer, 4)) << std::endl;
       
       if (calc_checksum(rx_buffer, 4) == rx_buffer[4])
         {
