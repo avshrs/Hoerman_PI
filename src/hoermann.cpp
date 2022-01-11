91bc563938417376384a22efb87bfe001ab5897f
avshrs <avshrs@gmail.com>
2022-01-11 01:16:21 +0100

    up

--- src/hoermann.cpp
+++ src/hoermann.cpp
@@ -64,13 +64,12 @@ bool Hoermann::read_rs232(void)
     if (buf[0] == SYNC_BYTE)
     {
       serial.serial_read(buf, 1);
-
-      
+      std::cout << " 0x"<<std::setw(2) << std::setfill('0')<<std::hex << static_cast<int>(buf[0]) << std::endl;
       
       serial.serial_read(buf, 5);
       
       for (int i = 0 ; i < 5 ;i++){
-        std::cout << " 0x"<<std::setw(2) << std::setfill('0')<<std::hex << static_cast<int>(buf[0]) << std::endl;
+        std::cout << " 0x"<<std::setw(2) << std::setfill('0')<<std::hex << static_cast<int>(buf[i]) << std::endl;
         rx_buffer[i] = buf[i];
       }
       if (calc_checksum(rx_buffer, 5) == rx_buffer[4])
