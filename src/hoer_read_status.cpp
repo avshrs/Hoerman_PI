bae1b12cd1e061b946311a78f14949afecec004c
avshrs <avshrs@gmail.com>
2022-01-12 21:19:59 +0100

    up

--- src/hoer_read_status.cpp
+++ src/hoer_read_status.cpp
@@ -12,16 +12,16 @@ void th1(){
 
 
 int main(){
-   
-   char serial_name[] = "/dev/ttyUSB0";
-   int boudrate = 19200;
-   door.init(serial_name, boudrate);
-   std::thread t3(th1);
-   sleep(60);
-   door.set_state("venting");
-   sleep(10);
-   door.set_state("close");
-   sleep(10);
-
+   while(1){
+      char serial_name[] = "/dev/ttyUSB0";
+      int boudrate = 19200;
+      door.init(serial_name, boudrate);
+      std::thread t3(th1);
+      sleep(60);
+      door.set_state("venting");
+      sleep(10);
+      door.set_state("close");
+      sleep(10);
+   }
 return 1;
 }
