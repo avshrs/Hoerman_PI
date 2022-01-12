be6c56ff3953f8d19cf4efd211a867c6d24d234b
avshrs <avshrs@gmail.com>
2022-01-13 00:14:04 +0100

    up

--- src/USB_serial.cpp
+++ src/USB_serial.cpp
@@ -137,7 +137,7 @@ void USB_serial::serial_send(uint8_t *data, int size)
 	{
 		buf[i] = static_cast<char>(data[i]);
 	}
-	write(fd, buf, size);
+	std::cout<< write(fd, buf, size) <<std::endl;
 }
 
 void USB_serial::serial_read(uint8_t *data, int size)
