6e455e429e2fc56be235025abb3c770a4fec59b3
avshrs <avshrs@gmail.com>
2022-01-12 20:54:21 +0100

    up

--- include/USB_serial.h
+++ include/USB_serial.h
@@ -5,7 +5,7 @@ class USB_serial
   public:
     int fd;
     void serial_open(char *serial_name, int baud);
-    // void serial_open2(const char *device, int baudrate, bool rtscts, struct termios *old);
+    void serial_open2(const char *device, int baudrate, bool rtscts, struct termios *old);
     void serial_send(uint8_t *data, int size);
     void serial_read(uint8_t *data, int size);
     uint8_t serial_read_byte(char *data, int timeout_usec);
