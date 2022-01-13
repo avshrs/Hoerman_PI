25137868e27e4e5557e75e4a941d360856d717cf
avshrs <avshrs@gmail.com>
2022-01-13 15:47:33 +0100

    up

--- src/USB_serial.cpp
+++ src/USB_serial.cpp
@@ -58,7 +58,7 @@ if(baud){
         newtio.c_lflag = 0;
          
         newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-        newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+        newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
         
         tcflush(fd, TCIFLUSH);
         tcsetattr(fd,TCSANOW,&newtio);
