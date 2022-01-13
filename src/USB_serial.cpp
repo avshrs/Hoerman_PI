346c9ec22799ee871ced20ef4bdd8dbeb5d2bca9
avshrs <avshrs@gmail.com>
2022-01-13 14:50:07 +0100

    up

--- src/USB_serial.cpp
+++ src/USB_serial.cpp
@@ -65,7 +65,7 @@ void USB_serial::serial_open(char *serial_name, int baud)
 //   newtermios.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT IN LINUX)
 //   newtermios.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT IN LINUX)
 
-  newtermios.c_cc[VTIME] = 1;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  newtermios.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
   newtermios.c_cc[VMIN] = 0;
   
   cfsetispeed(&newtermios,baud);
