1179b617e93f05d1710940816f33fb1ff51cc1a6
avshrs <avshrs@gmail.com>
2022-01-14 00:54:16 +0100

    up

--- include/hoermann.h
+++ include/hoermann.h
@@ -55,11 +55,11 @@ class Hoermann_pi{
         struct TX_Buffer{
             uint8_t buf[6]={0};
             uint8_t len=0;
-            std::chrono::high_resolution_clock received_time;
+            std::chrono::_V2::system_clock::time_point received_time;
         };
         struct RX_Buffer{
             uint8_t buf[6]={0};
-            std::chrono::high_resolution_clock received_time;
+            std::chrono::_V2::system_clock::time_point received_time;
         };
 
 
