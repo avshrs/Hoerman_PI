16fd9c1336922410874871c19b0a350d156f51b2
avshrs <avshrs@gmail.com>
2022-01-13 23:08:01 +0100

    up

--- src/hoer_read_status.cpp
+++ src/hoer_read_status.cpp
@@ -16,7 +16,16 @@ int main(){
    int boudrate = 19200;
    door.init(serial_name, boudrate);
    std::thread t3(th1);
+    sleep(60);
    while(1){
+   
+   door.set_state("toggle_light");
+   sleep(1);
+   std::cout<<door.get_state()<<std::endl;
+   sleep(1);
+   door.set_state("toggle_light");
+   sleep(1);
+   std::cout<<door.get_state()<<std::endl;
    sleep(1);
    }
 return 1;
