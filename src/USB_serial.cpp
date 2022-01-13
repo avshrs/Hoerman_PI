9781b52d9bd631c78c7fdf6cf37dea5f2916372c
avshrs <avshrs@gmail.com>
2022-01-13 11:52:18 +0100

    up

--- src/USB_serial.cpp
+++ src/USB_serial.cpp
@@ -135,7 +135,7 @@ void USB_serial::serial_open2(const char *device, int baudrate, bool rtscts, str
 
 void USB_serial::serial_send(uint8_t *data, int size)
 { 	char buf[15+3] = {0};
-	for(int i = 0; i< size; i++)
+	for(int i = 0; i< size+1; i++)
 	{
 		buf[i] = static_cast<char>(data[i]);
 	}
