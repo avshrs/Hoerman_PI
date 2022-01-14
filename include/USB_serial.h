321909551e8fa783e8092f659d15a0e0a22e38f9
avshrs <avshrs@gmail.com>
2022-01-15 00:32:10 +0100

    up

--- include/USB_serial.h
+++ include/USB_serial.h
@@ -8,9 +8,6 @@ class USB_serial
     int fd;
     
     void serial_open(const char* serial_name);
-    void set_cs8_19200();
-    void set_cs7_9600();
-    void serial_open2(const char *device, int baudrate, bool rtscts, struct termios *old);
     void serial_send(uint8_t *data, int size);
     void serial_read(uint8_t *data, int size);
     uint8_t serial_read_byte(char *data, int timeout_usec);
--- src/USB_serial.cpp
+++ src/USB_serial.cpp
@@ -1,71 +1,84 @@
+#include "USB_serial.h"
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string>
+#include <termios.h>
+#include <iomanip>
+
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <string.h>
-#include "USB_serial.h"
-
-// Linux headers
-#include <fcntl.h> // Contains file controls like O_RDWR
-#include <errno.h> // Error integer and strerror() function
-#include <termios.h> // Contains POSIX terminal control definitions
-#include <unistd.h> // write(), read(), close()
+#include <ctype.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#ifdef __linux__
 #include <linux/serial.h>
+#endif
 
 void USB_serial::serial_open(const char *serial_name)
 {
-  // Open the serial port. Change device path as needed (currently set to an standard FTDI USB-UART cable type device)
-  int fd = open(serial_name, O_RDWR);
+  struct termios newtermios;
+  fd = open(serial_name,O_RDWR | O_NOCTTY | O_NONBLOCK);
 
-  // Create new termios struc, we call it 'tty' for convention
-  struct termios tty;
-
-
-  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
-  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
-  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
-  tty.c_cflag |= CS8; // 8 bits per byte (most common)
-  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
-  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-
-  tty.c_lflag &= ~ICANON;
-  tty.c_lflag &= ~ECHO; // Disable echo
-  tty.c_lflag &= ~ECHOE; // Disable erasure
-  tty.c_lflag &= ~ECHONL; // Disable new-line echo
-  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
-
-  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-  // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
-  // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
-
-  tty.c_cc[VTIME] = 1;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-  tty.c_cc[VMIN] = 0;
-
-  // Set in/out baud rate to be 9600
-  cfsetispeed(&tty, B19200);
-  cfsetospeed(&tty, B19200);
-  
-  if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
-   
+  if (fd < 0) 
+  {
+    std::cout << "Error from open serial port" << fd << std::endl;
+  }   
+  if(tcgetattr(fd, &newtermios) != 0) 
+  {
+    std::cout << "Error from tcgetattr " << std::endl;
   }
+
+  newtermios.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+  // newtermios.c_cflag |= PARENB;  // Set parity bit, enabling parity
+  newtermios.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+  // newtermios.c_cflag |= CSTOPB;  // Set stop field, two stop bits used in communication
+  newtermios.c_cflag &= ~CSIZE; // Clear all the size bits, then use one of the statements below
+  // newtermios.c_cflag |= CS5; // 5 bits per byte
+  // newtermios.c_cflag |= CS6; // 6 bits per byte
+  // newtermios.c_cflag |= CS7; // 7 bits per byte
+  newtermios.c_cflag |= CS8; // 8 bits per byte (most common)
+  newtermios.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+  // newtermios.c_cflag |= CRTSCTS;  // Enable RTS/CTS hardware flow control
+  newtermios.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+  newtermios.c_lflag &= ~ICANON;
+  newtermios.c_lflag &= ~ECHO; // Disable echo
+  newtermios.c_lflag &= ~ECHOE; // Disable erasure
+  newtermios.c_lflag &= ~ECHONL; // Disable new-line echo
+  newtermios.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+  newtermios.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+  newtermios.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+  
+  newtermios.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+  newtermios.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+//   newtermios.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT IN LINUX)
+//   newtermios.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT IN LINUX)
+
+  newtermios.c_cc[VTIME] = 1;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  newtermios.c_cc[VMIN] = 0;
+  
+  cfsetispeed(&newtermios, B19200);
+  cfsetospeed(&newtermios, B19200);
+    
 }   
 
 
-
-
-
-
 void USB_serial::serial_send(uint8_t *data, int size)
-{  	char buf[15+3] = {0};
- 	for(int i = 0; i< size; i++)
- 	{ 
- 		buf[i] = static_cast<char>(data[i]);
- 	}
-
-	write(fd, buf, size);
+{  
+	write(fd, data, size);
 }
+
 
 void USB_serial::serial_read(uint8_t *data, int size)
 {	
@@ -77,3 +90,4 @@ void USB_serial::serial_close()
 {
    close(fd);
 }
+
