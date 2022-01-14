981c853a0fb1fc5f6d2c439ce5a54d901230df95
avshrs <avshrs@gmail.com>
2022-01-15 00:05:18 +0100

    up

--- include/USB_serial.h
+++ include/USB_serial.h
@@ -6,7 +6,7 @@ class USB_serial
 {
   public:
     int fd;
-    termios tty;
+    struct termios tty;
     void serial_open(const char* serial_name);
     void set_cs8_19200();
     void set_cs7_9600();
