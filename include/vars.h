8989c7029a4576bae4273dd4189898b2f53c5d8b
avshrs <avshrs@gmail.com>
2022-01-14 22:37:06 +0100

    up

--- include/vars.h
+++ include/vars.h
@@ -3,14 +3,13 @@
 #include <unistd.h>
 #include <chrono>
 #include <vector>
-std::size_t n = 18;
 struct TX_Buffer{
-    std::vector<uint8_t> buf = std::vector<uint8_t>(n, 0x00);
+    std::vector<uint8_t> buf = std::vector<uint8_t>(18, 0x00);
     uint8_t len=0;
     int timeout = 0;
 };
 struct RX_Buffer{
-    std::vector<uint8_t> buf = std::vector<uint8_t>(n, 0x00);
+    std::vector<uint8_t> buf = std::vector<uint8_t>(18, 0x00);
     // std::chrono::_V2::system_clock::time_point received_time;
 };
 
