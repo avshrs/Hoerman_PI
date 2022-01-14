e8aa571531daf181ac120cc6fbe91deb58cfc825
avshrs <avshrs@gmail.com>
2022-01-14 11:31:40 +0100

    up

--- src/hoermann.cpp
+++ src/hoermann.cpp
@@ -7,15 +7,15 @@
 #include <unistd.h>
 
 
-struct TX_Buffer{
-    uint8_t buf[6]={0};
-    uint8_t len=0;
-    std::chrono::_V2::system_clock::time_point received_time;
-};
-struct RX_Buffer{
-    uint8_t buf[6]={0};
-    std::chrono::_V2::system_clock::time_point received_time;
-};
+// struct TX_Buffer{
+//     uint8_t buf[6]={0};
+//     uint8_t len=0;
+//     std::chrono::_V2::system_clock::time_point received_time;
+// };
+// struct RX_Buffer{
+//     uint8_t buf[6]={0};
+//     std::chrono::_V2::system_clock::time_point received_time;
+// };
 
 
 
