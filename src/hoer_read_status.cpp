fa7fd5a9e8ad9e4c4dc5d2d7b83c50859be2e46c
avshrs <avshrs@gmail.com>
2022-01-14 23:15:17 +0100

    up

--- src/hoer_read_status.cpp
+++ src/hoer_read_status.cpp
@@ -7,14 +7,12 @@ Hoermann_pi door;
 
 
 void th1(){
-   door.run_loop();    
+   door.run_loop("/dev/ttyUSB1");    
 }
 
 
 int main(){
-   char serial_name[] = "/dev/ttyUSB1";
-   int boudrate = 19200;
-   door.init(serial_name, boudrate);
+   
    std::thread t3(th1);
     sleep(10);
    while(1){
