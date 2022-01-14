f7b8352b3d8a48a0415a0ec94cce6c498719cc56
avshrs <avshrs@gmail.com>
2022-01-14 22:53:39 +0100

    up

--- src/hoermann.cpp
+++ src/hoermann.cpp
@@ -166,7 +166,7 @@ void Hoermann_pi::make_scan_responce_msg(RX_Buffer* rx_buf, TX_Buffer* tx_buf)
     tx_buf->buf[4] = calc_crc8(tx_buf->buf.data(), 4);
     tx_buf->len = 5;
     // tx_buf.received_time = buf->received_time;
-    tx_buf->timeout = 30000;
+    tx_buf->timeout = 22000;
     
 }
 
@@ -181,7 +181,7 @@ void Hoermann_pi::make_status_req_msg(RX_Buffer* rx_buf, TX_Buffer* tx_buf)
     tx_buf->buf[5] = calc_crc8(tx_buf->buf.data(), 5);
     tx_buf->len = 6;
     // tx_buf.received_time = buf->received_time; 
-    tx_buf->timeout = 4100;
+    tx_buf->timeout = 16100;
 }
 
 
