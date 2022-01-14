c5c70bfdb1e1db6913f3cb624e07e1f6babdf129
avshrs <avshrs@gmail.com>
2022-01-14 11:28:52 +0100

    up

--- include/hoermann.h
+++ include/hoermann.h
@@ -23,6 +23,17 @@
 
 #define CRC8_INITIAL_VALUE        0xF3
 
+
+    struct TX_Buffer{
+        uint8_t buf[6]={0};
+        uint8_t len=0;
+        std::chrono::_V2::system_clock::time_point received_time;
+    };
+    struct RX_Buffer{
+        uint8_t buf[6]={0};
+        std::chrono::_V2::system_clock::time_point received_time;
+    };
+
 class Hoermann_pi{
     private:
         USB_serial serial;
