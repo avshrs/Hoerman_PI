3f0f2447300ae237af4ff45b6f03c67b6018853b
avshrs <avshrs@gmail.com>
2022-01-15 23:00:17 +0100

    up

--- src/hoermann.cpp
+++ src/hoermann.cpp
@@ -52,15 +52,15 @@ void Hoermann_pi::run_loop(void)
                     auto deltaTime = std::chrono::duration_cast<mi>(check - start).count();
                     if( deltaTime > (tx_buf->timeout) && deltaTime < max_frame_delay)
                     {   
-                        // std::cout << "--------------\n";
-                        // print_buffer(rx_buf->buf.data(),rx_buf->buf.size());
-                        // print_buffer(tx_buf->buf.data(),tx_buf->buf.size());
-                        // // std::cout << "--------------\n\n";
+                        std::cout << "--------------\n";
+                        print_buffer(rx_buf->buf.data(),rx_buf->buf.size());
+                        print_buffer(tx_buf->buf.data(),tx_buf->buf.size());
+                        // std::cout << "--------------\n\n";
                         serial.serial_send(tx_buf);
-                        // auto check2 = timer.now();
-                        // auto deltaTime2 = std::chrono::duration_cast<mi>(check2 - start).count();
+                        auto check2 = timer.now();
+                        auto deltaTime2 = std::chrono::duration_cast<mi>(check2 - start).count();
                         
-                        // std::cout << "-------"<<deltaTime2 <<"-------\n";
+                        std::cout << "-------"<<deltaTime2 <<"-------\n";
                         break;
                     }
                     usleep(10);
