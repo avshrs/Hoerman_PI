2ca8764eb1c3b2577b4774e2678d4cd17238814d
avshrs <avshrs@gmail.com>
2022-01-16 22:15:53 +0100

    up

--- src/hoer_read_status.cpp
+++ src/hoer_read_status.cpp
@@ -17,18 +17,19 @@ void th2(Mqtt_Client *mqtt){
 }
 
 int main(){
-   
    Config_manager cfg;
-   Hoermann_pi door;
    cfg.read_config();
+
+   Hoermann_pi door;
    std::string serial_file = cfg.get_hoer_serial_file();
    int rs_lead_zero = cfg.get_hoer_lead_zeros();
    int boudrate = cfg.get_hoer_boudrate();
 
    Mqtt_Client mqtt(cfg.get_mqtt_ClientId().c_str(), cfg.get_mqtt_ip().c_str(), cfg.get_mqtt_port(), cfg.get_mqtt_username().c_str(), cfg.get_mqtt_password().c_str());
-
-   mqtt.register_horman(&door);
    door.register_mqtt(&mqtt);
+   mqtt.register_horman(&door);
+   sleep(1);
+
    door.init(serial_file.c_str(), boudrate, rs_lead_zero);
    
    std::thread t3(th1, &door);
