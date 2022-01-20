4cbd303b1f48059a8b404703de243ec0064af37c
avshrs <avshrs@gmail.com>
2022-01-20 12:04:56 +0100

    up

--- src/logger.cpp
+++ src/logger.cpp
@@ -32,3 +32,39 @@ void Logger::save()
 
 
 
+
+class Logger {
+    private:
+        ostringstream oss;
+    public:
+        template <typename T>
+        Logger& operator<<(T a);
+
+    Logger& operator<<( std::ostream&(*f)(std::ostream&) )
+    {
+        if( f == std::endl )
+        {
+            std::cout << "12-09-2009 11:22:33" << oss.str() << std::endl;   
+            oss.str("");
+        }
+        return *this;
+    }
+};
+
+template <typename T>
+Logger& Logger::operator<<(T a) {
+    oss << a;
+    return *this;
+}
+
+void functionTest(void) {
+    Logger log;
+    log << "Error: " << 5 << " seen" << std::endl;
+}
+
+int main()
+{
+    functionTest();
+}
+
+
