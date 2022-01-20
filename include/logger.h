8e6beaf1f9583bec605f55d1ce668222943ce1f1
avshrs <avshrs@gmail.com>
2022-01-20 10:51:51 +0100

    up

--- include/logger.h
+++ include/logger.h
@@ -15,7 +15,7 @@ class Logger
     Logger& operator<< (const T& str)
     {
         out << str;
-        if(str == static_cast<std::string>("endl"))
+        if(out > 100)
           save();
         return *this;
     }
